01a8ba15e98f4e3d4faeb5e6dd986f6649550454
anushayadav01 <Anusha_Yadav@mentor.com>
2022-12-29 09:31:20 +0000

    added files

---
+++ fastapi_server/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#-------------------------------------------------------------
+#Setup script for MTDA-FastAPI
+#-------------------------------------------------------------
+from setuptools import setup, find_packages
+  
+setup(
+        name='mtda_fastapi',
+        packages=find_packages(),
+        version='1.0.0',
+        scripts=['mtda-fastapi-cli'],
+        description='Multi-Tenant Devices Access-FASTAPI',
+        url='https://github.com/anushayadav01/mtda_fastapi',
+        classifiers=[
+            "Topic :: Utilities",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.0",
+            "Topic :: Software Development :: Embedded Systems",
+            ],
+
+        install_requires=[
+            "python-daemon>=2.0",
+            "fastapi_websocket_rpc",
+            "fastapi>=0.78.0,<1",
+            "pydantic>=1.9.1,<2",
+            "uvicorn>=0.17.6,<1",
+            "websockets>=10.3,<11",
+            "tenacity>=8.0.1,<9",
+            "asyncinit",
+            "pyzmq>=15.0,<23.0.0"
+            ],
+ )
+        
